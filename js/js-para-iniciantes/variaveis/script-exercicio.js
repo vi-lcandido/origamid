2afa45cf883847b11d3b4e8598250906b85d7560
vi-lcandido <vitoria.lcandido@gmail.com>
2023-08-24 23:16:37 -0300

    201 feat - variaveis

---
+++ js/js-para-iniciantes/variaveis/script-exercicio.js
@@ -0,0 +1,23 @@
+// Declarar uma variável com o seu nome
+var nome = "Vitória";
+// Declarar uma variável com a sua idade
+var idade = 27;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "árabe";
+
+// Declarar 5 variáveis diferentes sem valores
+var pais, continente, viagemFavorita, objetivo, casa;
+
+console.log(
+  nome,
+  idade,
+  comidaFavorita,
+  pais,
+  continente,
+  viagemFavorita,
+  objetivo,
+  casa
+);
