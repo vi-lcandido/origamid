679d75d32c420723d2e27866049bf263b89cf107
vi-lcandido <vitoria.lcandido@gmail.com>
2023-08-27 16:36:38 -0300

    Aula 202: tipos de dados

---
+++ js/js-para-iniciantes/tipos-de-dados/script-exercicio.js
@@ -0,0 +1,22 @@
+// Declare uma variável contendo uma string
+var cidade = "Berlim";
+
+// Declare uma variável contendo um número dentro de uma string
+var populacao = "3645000000";
+
+// Declare uma variável com a sua idade
+var idade = 27;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Zé";
+var sobrenome = "Ninguém";
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(`nome completo é ${nomeCompleto}`);
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+var verificaTipoNome = typeof nome;
+console.log(` O tipo do meu nome é: ${verificaTipoNome}`);
