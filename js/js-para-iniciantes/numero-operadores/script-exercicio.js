155c31451c74ff7f60d74d721531977fa4bd8314
vi-lcandido <vitoria.lcandido@gmail.com>
2023-08-27 19:06:34 -0300

    Aula 203: numeros e operadores

---
+++ js/js-para-iniciantes/numero-operadores/script-exercicio.js
@@ -0,0 +1,27 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+
+console.log(`O valor total é ${total}`);
+
+// Crie duas expressões que retornem NaN
+var numero1 = "s10";
+var expressãoNan = numero1 / 10;
+
+console.log(`expressão NaN: ${expressãoNan}`);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +"200" + 50;
+
+console.log(soma);
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero2 = 5;
+
+console.log(++numero2);
+
+// Como dividir o peso por 2?
+var numero = +"80" / 2;
+var unidade = "kg";
+var peso = numero + unidade; // '80kg'
+// var pesoPorDois = peso / 2; // NaN (Not a Number)
+
+console.log(peso);
