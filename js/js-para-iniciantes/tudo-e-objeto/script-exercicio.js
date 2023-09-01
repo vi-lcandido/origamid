7ec738e7871b3cedd6b4df2761230dc24657404b
vi-lcandido <vitoria.lcandido@gmail.com>
2023-08-31 21:20:50 -0300

    Aula 207: tudo é objeto

---
+++ js/js-para-iniciantes/tudo-e-objeto/script-exercicio.js
@@ -0,0 +1,18 @@
+// nomeie 3 propriedades ou métodos de strings
+toString();
+charAt();
+length;
+slice();
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector(".btn");
+addEventListener;
+appendChild;
+getElementsByClassName;
+innerHtml;
+id;
+outerHtml;
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// API -> https://clipboardjs.com/
