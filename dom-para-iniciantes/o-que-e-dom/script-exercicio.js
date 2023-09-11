e96d385651e936a4913194221ac1974a9228700c
vi-lcandido <vitoria.lcandido@gmail.com>
2023-09-10 22:07:48 -0300

    Aula 301 - o que é DOM

---
+++ dom-para-iniciantes/o-que-e-dom/script-exercicio.js
@@ -0,0 +1,18 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+
+console.log(elementoAtivo);
+
+// Retorne a linguagem do navegador
+const linguagemNavegador = navigator.language;
+
+console.log(linguagemNavegador);
+// Retorne a largura da janela
+
+const larguraPagina = window.innerWidth;
+
+console.log(larguraPagina);
