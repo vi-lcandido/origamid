06a4e4ec02efa288d2272447cb77c0b31b6cc8e7
vi-lcandido <vitoria.lcandido@gmail.com>
2024-02-17 17:42:04 -0300

    Aula 203 - union types

---
+++ ts-para-iniciantes/union-types/script.js
@@ -0,0 +1,18 @@
+"use strict";
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === "number") {
+        return value;
+    }
+    else if (typeof value === "string") {
+        return Number(value);
+    }
+    else {
+        throw "value deve ser um número | string";
+    }
+}
+console.log(toNumber("300"));
