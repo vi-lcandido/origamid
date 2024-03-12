7b7f2f9e8c5fa8019f185e097ed6e5797e7d18dc
vi-lcandido <vitoria.lcandido@gmail.com>
2024-03-11 22:43:12 -0300

    Aula302 - interfaces do DOM, classe geradora do elemento e de onde herdam

---
+++ ts-para-iniciantes/interfaces-do-DOM/exercicio.ts
@@ -0,0 +1,20 @@
+// 1 - Selecione os elementos com a classe link.
+
+// 2 - Crie uma função que deve ser executada para cada elemento.
+
+// 3 - Modificar através da função o estilo da color e border.
+
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  //HTMLAnchorElement e HTMLButtonElement herdam de HTMLElement então ambos serão ativados na minha fç
+  if (link instanceof HTMLElement) {
+    mudarCorEBorda(link);
+  }
+});
+
+//para mudar a cor e borda de element, ou seja, alterar o style, portanto ele precisa ser um HTMLElement para alterar style da tag a e button
+function mudarCorEBorda(element: HTMLElement) {
+  element.style.border = "2px solid purple";
+  element.style.color = "red";
+}
